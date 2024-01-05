e8733b5569e3144cedd2e74436b85f27548d20f5
Loïc <105038676+MayranL@users.noreply.github.com>
2024-01-05 22:01:45 +0100

    [Firebase/User] Get user information from Firebase and show it on the HomePage.

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,8 +1,10 @@
 import 'package:chat_app/components/my_drawer.dart';
 import 'package:chat_app/services/auth/auth_service.dart';
 import 'package:chat_app/services/chat/chat_service.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+
+import '../components/user_tile.dart';
+import 'chat_page.dart';
 
 class HomePage extends StatelessWidget {
   HomePage({super.key});
@@ -18,6 +20,43 @@ class HomePage extends StatelessWidget {
       backgroundColor: Theme.of(context).colorScheme.primary,
       ),
       drawer: MyDrawer(),
+      body: _buildUserList(),
     );
+  }
+
+  // build a list of users except for the current logged in user
+  Widget _buildUserList(){
+    return StreamBuilder(stream: _chatService.getUsersStream(), builder: (context,snapshot){
+      // error
+      if (snapshot.hasError){
+        return const Text("Error");
+      }
+
+      // loading
+      if (snapshot.connectionState == ConnectionState.waiting){
+        return const Text("Loading..");
+      }
+
+      // return list view
+      return ListView(
+        children: snapshot.data!.map<Widget>((userData) => _buildUserListItem(userData,context)).toList(),
+      );
+    });
+  }
+
+  // build individual list tile for user
+  Widget _buildUserListItem(Map<String,dynamic> userData, BuildContext context){
+    // display all users except current user
+    if (userData["email"] != _authService.getCurrentUser()!.email) {
+      return UserTile(
+        text: userData["email"],
+        onTap: () {
+          // tapped on a user -> go to chat page
+          Navigator.push(context, MaterialPageRoute(builder: (context) => ChatPage(receiverEmail: userData["email"])));
+        },
+      );
+    } else {
+      return Container();
+    }
   }
 }
--- lib/services/auth/auth_service.dart
+++ lib/services/auth/auth_service.dart
@@ -1,14 +1,29 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthService {
   // instace of auth
   final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  User? getCurrentUser() {
+    return _auth.currentUser;
+  }
 
   // sign in
   Future<UserCredential> signInWithEmailPassword(String email, password) async {
     try {
       UserCredential userCredential = await _auth.signInWithEmailAndPassword(
           email: email, password: password);
+
+      // save user info if it doesn't already exist
+      _firestore.collection("Users").doc(userCredential.user!.uid).set(
+          {
+            'uid': userCredential.user!.uid,
+            'email': email,
+          }
+      );
+
       return userCredential;
     } on FirebaseAuthException catch (e) {
       throw Exception(e.code);
@@ -20,6 +35,16 @@ class AuthService {
     try {
       UserCredential userCredential = await _auth.createUserWithEmailAndPassword(
           email: email, password: password);
+
+      // save user info
+      _firestore.collection("Users").doc(userCredential.user!.uid).set(
+        {
+          'uid': userCredential.user!.uid,
+          'email': email,
+        }
+      );
+
+
       return userCredential;
     } on FirebaseAuthException catch (e) {
       throw Exception(e.code);
