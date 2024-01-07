051b41cad7d540a8f8d7b906e4d4d905f448b6e6
Loïc <105038676+MayranL@users.noreply.github.com>
2024-01-07 16:31:46 +0100

    [Firebase/message] Add message and chat_rooms with firebase. Everything working !

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -16,8 +16,9 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Home"),
-      backgroundColor: Theme.of(context).colorScheme.primary,
+      appBar: AppBar(
+        title: Text("Home"),
+        backgroundColor: Theme.of(context).colorScheme.primary,
       ),
       drawer: MyDrawer(),
       body: _buildUserList(),
@@ -25,34 +26,44 @@ class HomePage extends StatelessWidget {
   }
 
   // build a list of users except for the current logged in user
-  Widget _buildUserList(){
-    return StreamBuilder(stream: _chatService.getUsersStream(), builder: (context,snapshot){
-      // error
-      if (snapshot.hasError){
-        return const Text("Error");
-      }
+  Widget _buildUserList() {
+    return StreamBuilder(
+        stream: _chatService.getUsersStream(),
+        builder: (context, snapshot) {
+          // error
+          if (snapshot.hasError) {
+            return const Text("Error");
+          }
 
-      // loading
-      if (snapshot.connectionState == ConnectionState.waiting){
-        return const Text("Loading..");
-      }
+          // loading
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Text("Loading..");
+          }
 
-      // return list view
-      return ListView(
-        children: snapshot.data!.map<Widget>((userData) => _buildUserListItem(userData,context)).toList(),
-      );
-    });
+          // return list view
+          return ListView(
+            children: snapshot.data!
+                .map<Widget>(
+                    (userData) => _buildUserListItem(userData, context))
+                .toList(),
+          );
+        });
   }
 
   // build individual list tile for user
-  Widget _buildUserListItem(Map<String,dynamic> userData, BuildContext context){
+  Widget _buildUserListItem(
+      Map<String, dynamic> userData, BuildContext context) {
     // display all users except current user
     if (userData["email"] != _authService.getCurrentUser()!.email) {
       return UserTile(
         text: userData["email"],
         onTap: () {
           // tapped on a user -> go to chat page
-          Navigator.push(context, MaterialPageRoute(builder: (context) => ChatPage(receiverEmail: userData["email"])));
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) =>
+                      ChatPage(receiverEmail: userData["email"], receiverID: userData["uid"],)));
         },
       );
     } else {
